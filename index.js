09c4dd4425ff9b363a8e05a9e0d98b79ab13ace9
JonathanWekesser <118290713+JonathanWekesser@users.noreply.github.com>
2023-10-14 16:46:32 +0200

    ende

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get("/products", function (req, res) {
         res.writeHead(200, {
             "Content-Type": "application/json",
         });
-        res.end(JSON.stringify(data));
+        res.end(data);
     });
 });
 
